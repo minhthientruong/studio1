373a54036b7761444bae943396aec553085a881f
Thiện Trương <thienvagy@gmail.com>
2023-05-04 00:57:35 +0700

    first commit

---
+++ src/components/Guides.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/Button'
+import { Heading } from '@/components/Heading'
+
+
+
+export function Guides() {
+  return (
+    <div className="my-16 xl:max-w-none">
+      <Heading level={2} id="guides">
+Từ Cấm Facebook
+      </Heading>
+
+    </div>
+  )
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+export function Logo(props) {
+  return (
+    <div className=" flex  mx-4 w-full h-full ">
+      <img className="rounded-xl w-14 " src="/logo.png">
+      </img>
+    <span className=" font-bold text-xl my-auto dark:text-white px-4 whitespace-no-wrap">SuZu Studio</span>
+    </div>
+  )
+}
